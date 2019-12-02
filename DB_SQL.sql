92885c2f316fa239317127a08fdb2fbeaeb18c72
StephenJohnson42 <54747618+StephenJohnson42@users.noreply.github.com>
2019-12-02 18:44:07 -0500

    Update DB_SQL.sql

--- DB_SQL.sql
+++ DB_SQL.sql
@@ -10,15 +10,24 @@ CREATE TABLE IF NOT EXISTS `tvOrMovie` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `tvOrMovie` (`title`, `rating`, `year`,'pcompany') VALUES
-('The Shining', 8.4, 1980, 'Warner Bros.'),
-('Great Expectations', 7.8, 1946, 'Cineguild'),
-('The Age Of Innocence', 7.2, 1993, 'Columbia Pictures'),
-('To Kill A MockingBird', 8.3, 1962, 'Universal Pictures'),
-('The Godfather', 9.2, 1972, 'Paramount Pictures'),
-('American Psycho', 7.6, 2000, 'LionsGate'),
-('The Silence of the Lambs', 8.6, 1991, 'Orion'),
-('Wuthering Heights', 7.6, 1939, 'United Artists'),
-('Moby Dick', 7.3, 1956, 'Warner Bros.'),
+('The Shining', 8.4, 1980, 'Warner Bros.');
+INSERT INTO `tvOrMovie` (`title`, `rating`, `year`,'pcompany') VALUES
+('Great Expectations', 7.8, 1946, 'Cineguild');
+INSERT INTO `tvOrMovie` (`title`, `rating`, `year`,'pcompany') VALUES
+('The Age Of Innocence', 7.2, 1993, 'Columbia Pictures');
+INSERT INTO `tvOrMovie` (`title`, `rating`, `year`,'pcompany') VALUES
+('To Kill A MockingBird', 8.3, 1962, 'Universal Pictures');
+INSERT INTO `tvOrMovie` (`title`, `rating`, `year`,'pcompany') VALUES
+('The Godfather', 9.2, 1972, 'Paramount Pictures');
+INSERT INTO `tvOrMovie` (`title`, `rating`, `year`,'pcompany') VALUES
+('American Psycho', 7.6, 2000, 'LionsGate');
+INSERT INTO `tvOrMovie` (`title`, `rating`, `year`,'pcompany') VALUES
+('The Silence of the Lambs', 8.6, 1991, 'Orion');
+INSERT INTO `tvOrMovie` (`title`, `rating`, `year`,'pcompany') VALUES
+('Wuthering Heights', 7.6, 1939, 'United Artists');
+INSERT INTO `tvOrMovie` (`title`, `rating`, `year`,'pcompany') VALUES
+('Moby Dick', 7.3, 1956, 'Warner Bros.');
+INSERT INTO `tvOrMovie` (`title`, `rating`, `year`,'pcompany') VALUES
 ('No Country For Old Men', 8.1, 2007, 'Paramount Pictures');
 
 CREATE TABLE IF NOT EXISTS `book` (
@@ -30,15 +39,24 @@ CREATE TABLE IF NOT EXISTS `book` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `book` (`title`, `rating`, `author`,'pcompany') VALUES
-('The Shining', 8.4, 'Stephen King','New English Library'),
-('Great Expectations', 7.5, 'Charles Dickens', 'Oxford University Press'),
-('The Age Of Innocence', 7.8, 'Edith Wharton', 'Penguin Books'),
-('To Kill A MockingBird', 8.6, 'Harper Lee', 'HarperCollins'),
-('The Godfather', 8.8, 'Mario Puzo', 'New American Library'),
-('American Psycho', 7.6, 'Bret Easton Ellis', 'Penguin Books'),
-('The Silence of the Lambs', 8.4, 'Thomas Harris', 'St. Martins Press'),
-('Wuthering Heights', 7.9, 'Emily Bronte', 'Penguin Books'),
-('Moby Dick', 7.0, 'Herman Melville', 'Pengiun Books'),
+('The Shining', 8.4, 'Stephen King','New English Library');
+INSERT INTO `book` (`title`, `rating`, `author`,'pcompany') VALUES
+('Great Expectations', 7.5, 'Charles Dickens', 'Oxford University Press');
+INSERT INTO `book` (`title`, `rating`, `author`,'pcompany') VALUES
+('The Age Of Innocence', 7.8, 'Edith Wharton', 'Penguin Books');
+INSERT INTO `book` (`title`, `rating`, `author`,'pcompany') VALUES
+('To Kill A MockingBird', 8.6, 'Harper Lee', 'HarperCollins');
+INSERT INTO `book` (`title`, `rating`, `author`,'pcompany') VALUES
+('The Godfather', 8.8, 'Mario Puzo', 'New American Library');
+INSERT INTO `book` (`title`, `rating`, `author`,'pcompany') VALUES
+('American Psycho', 7.6, 'Bret Easton Ellis', 'Penguin Books');
+INSERT INTO `book` (`title`, `rating`, `author`,'pcompany') VALUES
+('The Silence of the Lambs', 8.4, 'Thomas Harris', 'St. Martins Press');
+INSERT INTO `book` (`title`, `rating`, `author`,'pcompany') VALUES
+('Wuthering Heights', 7.9, 'Emily Bronte', 'Penguin Books');
+INSERT INTO `book` (`title`, `rating`, `author`,'pcompany') VALUES
+('Moby Dick', 7.0, 'Herman Melville', 'Pengiun Books');
+INSERT INTO `book` (`title`, `rating`, `author`,'pcompany') VALUES
 ('No Country For Old Men', 8.3, 'Cormac McCarthy', 'Vintage');
 
 CREATE TABLE IF NOT EXISTS `pCompany` (
@@ -48,20 +66,34 @@ CREATE TABLE IF NOT EXISTS `pCompany` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `pCompany` (`name`, `location`) VALUES
-('New English Library', 'London'),
-('Warner Bros.', 'Burbank'),
-('Cineguild', 'London'),
-('Oxford University Press', 'Oxford'),
-('Columbia Pictures', 'Los Angeles'),
-('Penguin Books','London'),
-('Universal Pictures', 'New York City'),
-('HarperCollins', 'New York City'),
-('Paramount Pictures', 'Hollywood'),
-('New American Library', 'New York City'),
-('LionsGate', 'Santa Monica'),
-('Orion', 'Los Angeles'),
-('St. Martins Press', 'New York City'),
-('United Artists', 'Los Angeles'),
+('New English Library', 'London');
+INSERT INTO `pCompany` (`name`, `location`) VALUES
+('Warner Bros.', 'Burbank');
+INSERT INTO `pCompany` (`name`, `location`) VALUES
+('Cineguild', 'London');
+INSERT INTO `pCompany` (`name`, `location`) VALUES
+('Oxford University Press', 'Oxford');
+INSERT INTO `pCompany` (`name`, `location`) VALUES
+('Columbia Pictures', 'Los Angeles');
+INSERT INTO `pCompany` (`name`, `location`) VALUES
+('Penguin Books','London');
+INSERT INTO `pCompany` (`name`, `location`) VALUES
+('Universal Pictures', 'New York City');
+INSERT INTO `pCompany` (`name`, `location`) VALUES
+('HarperCollins', 'New York City');
+INSERT INTO `pCompany` (`name`, `location`) VALUES
+('Paramount Pictures', 'Hollywood');
+INSERT INTO `pCompany` (`name`, `location`) VALUES
+('New American Library', 'New York City');
+INSERT INTO `pCompany` (`name`, `location`) VALUES
+('LionsGate', 'Santa Monica');
+INSERT INTO `pCompany` (`name`, `location`) VALUES
+('Orion', 'Los Angeles');
+INSERT INTO `pCompany` (`name`, `location`) VALUES
+('St. Martins Press', 'New York City');
+INSERT INTO `pCompany` (`name`, `location`) VALUES
+('United Artists', 'Los Angeles');
+INSERT INTO `pCompany` (`name`, `location`) VALUES
 ('Vintage', 'New York City');
 
 
